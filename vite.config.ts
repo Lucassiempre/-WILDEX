2ff76a44a5b0a29a1f35bc63cdb331b57a0c05d7
Lucas Lionel Solohaga <106818513+Lucassiempre@users.noreply.github.com>
2026-09-19 11:21:14 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
-
 export default defineConfig({
-  base: repositoryName ? `/${repositoryName}/` : "./",
+  base: "/-WILDEX/",
   plugins: [react()],
 });
